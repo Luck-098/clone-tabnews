31b319a0b42978500d1f1a6632c934c49dbc3f05
Luck <lukasferreira455@gmail.com>
2026-04-16 14:32:18 +0000

    Página inicial

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 function home(){
-   return <div> <h1>Lorany eu te amo muito</h1> 
-          <p>Você é maravilhosa</p> </div>
+   return <div> 
+         <h1>Lorany eu te amo muito</h1> 
+          <p>Você é maravilhosa</p> 
+          </div>
 }
 
 export default home;
